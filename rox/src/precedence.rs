1e12db6e0105d079d7e76c1fd7a265a41a6261d1
Patrick <patches72790@gmail.com>
2022-03-11 21:27:38 -0600

    added u8 repr implementation of precedence

--- rox/src/precedence.rs
+++ rox/src/precedence.rs
@@ -1,6 +1,87 @@
 extern crate precedence_macro;
 use precedence_macro::make_precedence;
 
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+#[repr(u8)]
+pub enum NewPrecedence {
+    PrecNone = 0,
+    PrecAssign = 1,
+    PrecOr = 2,
+    PrecAnd = 3,
+    PrecEquality = 4,
+    PrecComparison = 5,
+    PrecTerm = 6,
+    PrecFactor = 7,
+    PrecUnary = 8,
+    PrecCall = 9,
+    PrecPrimary = 10,
+}
+
+impl Into<u8> for NewPrecedence {
+    fn into(self) -> u8 {
+        match self {
+            NewPrecedence::PrecNone => 0,
+            NewPrecedence::PrecAssign => 1,
+            NewPrecedence::PrecOr => 2,
+            NewPrecedence::PrecAnd => 3,
+            NewPrecedence::PrecEquality => 4,
+            NewPrecedence::PrecComparison => 5,
+            NewPrecedence::PrecTerm => 6,
+            NewPrecedence::PrecFactor => 7,
+            NewPrecedence::PrecUnary => 8,
+            NewPrecedence::PrecCall => 9,
+            NewPrecedence::PrecPrimary => 10,
+        }
+    }
+}
+
+impl From<u8> for NewPrecedence {
+    fn from(n: u8) -> Self {
+        match n {
+            0 => NewPrecedence::PrecNone,
+            1 => NewPrecedence::PrecAssign,
+            2 => NewPrecedence::PrecOr,
+            3 => NewPrecedence::PrecAnd,
+            4 => NewPrecedence::PrecEquality,
+            5 => NewPrecedence::PrecComparison,
+            6 => NewPrecedence::PrecTerm,
+            7 => NewPrecedence::PrecFactor,
+            8 => NewPrecedence::PrecUnary,
+            9 => NewPrecedence::PrecCall,
+            10 => NewPrecedence::PrecPrimary,
+            unknown => panic!("Cannot yield u8 from unknown Precedence Value {}", unknown),
+        }
+    }
+}
+
+impl std::ops::Add<u8> for NewPrecedence {
+    type Output = NewPrecedence;
+
+    fn add(self, rhs: u8) -> Self::Output {
+        let n: u8 = self.into();
+
+        NewPrecedence::from(n + rhs)
+    }
+}
+
+impl std::fmt::Display for NewPrecedence {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            NewPrecedence::PrecNone => write!(f, "PrecNone"),
+            NewPrecedence::PrecAssign => write!(f, "PrecAssign"),
+            NewPrecedence::PrecOr => write!(f, "PrecOr"),
+            NewPrecedence::PrecAnd => write!(f, "PrecAnd"),
+            NewPrecedence::PrecEquality => write!(f, "PrecEquality"),
+            NewPrecedence::PrecComparison => write!(f, "PrecComparison"),
+            NewPrecedence::PrecTerm => write!(f, "PrecTerm"),
+            NewPrecedence::PrecFactor => write!(f, "PrecFactor"),
+            NewPrecedence::PrecUnary => write!(f, "PrecUnary"),
+            NewPrecedence::PrecCall => write!(f, "PrecCall"),
+            NewPrecedence::PrecPrimary => write!(f, "PrecPrimary"),
+        }
+    }
+}
+
 #[derive(PartialOrd, PartialEq, Debug)]
 #[make_precedence(0)]
 pub struct PrecNone;
